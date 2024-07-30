3930681dfbc10e71fc945d88d7075700c90656d5
Zane Lu <zanelogi@gmail.com>
2024-07-30 15:23:07 +0800

    A demo to play audio.

---
+++ audio/index.js
@@ -0,0 +1,47 @@
+"use strict"
+
+let audioCtx;
+let buffer;
+let source;
+
+// Get UI elements
+const play = document.getElementById("play");
+const stop = document.getElementById("stop");
+
+play.addEventListener("click", async () => {
+    if (!audioCtx) {
+        audioCtx = new AudioContext();
+        await loadAudio();
+    } else {
+        playBuffer();
+    }
+});
+      
+stop.addEventListener("click", () => {
+    source.stop();
+    play.disabled = false;
+    stop.disabled = true;
+});
+
+function playBuffer() {
+    source = audioCtx.createBufferSource();
+    source.buffer = buffer;
+    source.connect(audioCtx.destination);
+    source.start();
+    play.disabled = true;
+    stop.disabled = false;
+}
+
+async function loadAudio() {
+    try {
+        const response = await fetch("sounds/tune0.wav");
+        audioCtx.decodeAudioData(await response.arrayBuffer(), (buf) => {
+            // executes when buffer has been decoded
+            buffer = buf;
+            play.disabled = false; // buffer loaded, enable play button
+            playBuffer();
+        });
+    } catch (err) {
+        console.error(`Unable to fetch the audio file. Error: ${err.message}`);
+    }
+}
---
+++ audio/readme.md
@@ -0,0 +1,9 @@
+Web Audio API examples: decodeAudioData() with callback
+
+Refer to https://developer.mozilla.org/en-US/docs/Web/API/BaseAudioContext/decodeAudioData
+
+To run this page
+1. run a http server in the project root folder => python -m http.server -b 127.0.0.1 8080
+2. open the url in a browser => http://127.0.0.1:8080/audio/
+
+Note: to exit the http server, press ctrl + c
