bed3be824ca5d906a19073bffdb6287c626e762c
Zane Lu <zanelogi@gmail.com>
2025-09-16 18:15:57 +0800

    [mairo] refactor map construction

--- mario/img.js
+++ mario/img.js
@@ -1,18 +1,27 @@
 import { resource } from "./resource.js";
 
 export class IMG {
+  #red_id;
   #image;
 
   constructor(res_id) {
-    this.#image = resource["res/images/" + res_id];
-    console.assert(this.#image, "No resource " + res_id);
+    this.#red_id = res_id;
+  }
+
+  get image() {
+    if (!this.#image) {
+      this.#image = resource["res/images/" + this.#red_id];
+      console.assert(this.#image, "No resource " + this.#red_id);
+    }
+    return this.#image;
   }
 
   draw(ctx, x, y, mirror) {
     mirror = mirror || false;
 
     if (!mirror) {
-      ctx.drawImage(this.#image, x, y);
+      const image = this.image;
+      ctx.drawImage(image, x, y);
     } else {
       this.drawFlipH(ctx, x, y);
     }
@@ -24,19 +33,21 @@ export class IMG {
 
   drawFlipV(ctx, x, y) {
     // flip image vertically
+    const image = this.image;
     ctx.save();
     ctx.scale(1, -1);
-    ctx.translate(0, -this.#image.height);
-    ctx.drawImage(this.#image, x, -y);
+    ctx.translate(0, -image.height);
+    ctx.drawImage(image, x, -y);
     ctx.restore();
   }
 
   drawFlipH(ctx, x, y) {
     // flip image horizontally
+    const image = this.image;
     ctx.save();
     ctx.scale(-1, 1);
-    ctx.translate(-this.#image.width, 0);
-    ctx.drawImage(this.#image, -x, y);
+    ctx.translate(-image.width, 0);
+    ctx.drawImage(image, -x, y);
     ctx.restore();
   }
 }
